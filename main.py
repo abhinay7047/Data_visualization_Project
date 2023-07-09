c4c0a71d5380b4324b5befc9e2277d26b2d976ed
abhinay7047 <dubeyabhinay999@gmail.com>
2023-07-09 13:45:56 +0530

    changeing_color_scale

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if plot:
     if selected_state == 'Overall India':
         # plot for india
         fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=4,size_max=35,
-                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District', color_continuous_scale='viridis')
+                                mapbox_style="carto-positron",width=1200,height=700,hover_name='District', color_continuous_scale='icefire')
 
         st.plotly_chart(fig,use_container_width=True)
     else:
@@ -38,6 +38,6 @@ if plot:
         state_df = df[df['State'] == selected_state]
 
         fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=6, size_max=35,
-                                mapbox_style="carto-positron", width=1200, height=700,hover_name='District', color_continuous_scale='viridis')
+                                mapbox_style="carto-positron", width=1200, height=700,hover_name='District', color_continuous_scale="icefire")
 
         st.plotly_chart(fig, use_container_width=True)
